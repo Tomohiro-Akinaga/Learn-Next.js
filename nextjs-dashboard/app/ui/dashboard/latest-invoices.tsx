e2292f6a368b982987e305b804ec56e990165ea1
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-03-11 00:29:53 +0800

    chapter9 wip

--- nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
+++ nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
@@ -2,8 +2,11 @@ import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
-import { LatestInvoice } from "@/app/lib/definitions";
-export default async function LatestInvoices({ latestInvoices }: { latestInvoices: LatestInvoice[] }) {
+import { fetchLatestInvoices } from "@/app/lib/data";
+
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className='flex w-full flex-col md:col-span-4'>
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Latest Invoices</h2>
@@ -17,7 +20,13 @@ export default async function LatestInvoices({ latestInvoices }: { latestInvoice
                   "border-t": i !== 0,
                 })}>
                 <div className='flex items-center'>
-                  <Image src={invoice.image_url} alt={`${invoice.name}'s profile picture`} className='mr-4 rounded-full' width={32} height={32} />
+                  <Image
+                    src={invoice.image_url}
+                    alt={`${invoice.name}'s profile picture`}
+                    className='mr-4 rounded-full'
+                    width={32}
+                    height={32}
+                  />
                   <div className='min-w-0'>
                     <p className='truncate text-sm font-semibold md:text-base'>{invoice.name}</p>
                     <p className='hidden text-sm text-gray-500 sm:block'>{invoice.email}</p>
--- nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
+++ nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,7 @@ import { generateYAxis } from "@/app/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 import { Revenue } from "@/app/lib/definitions";
+import { fetchRevenue } from "@/app/lib/data";
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,9 +10,9 @@ import { Revenue } from "@/app/lib/definitions";
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) {
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue();
   const chartHeight = 350;
-  // NOTE: Uncomment this code in Chapter 7
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
@@ -22,11 +23,12 @@ export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) 
   return (
     <div className='w-full md:col-span-4'>
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Recent Revenue</h2>
-      {/* NOTE: Uncomment this code in Chapter 7 */}
 
       <div className='rounded-xl bg-gray-50 p-4'>
         <div className='sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4'>
-          <div className='mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex' style={{ height: `${chartHeight}px` }}>
+          <div
+            className='mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex'
+            style={{ height: `${chartHeight}px` }}>
             {yAxisLabels.map(label => (
               <p key={label}>{label}</p>
             ))}
